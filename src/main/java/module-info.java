a00203222109f9d3977dbf7ba4929011648470e5
Marc Arndt <marc.arndt@masimi.de>
2019-02-11 15:01:18 +0100

    - rename module to org.phoenicis.javafx.collections

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module javafx.collections {
+module org.phoenicis.javafx.collections {
     exports org.phoenicis.javafx.collections;
     opens org.phoenicis.javafx.collections;
     requires com.google.common;
